c734f5bff845035a8a25cb24cec5ab4562b60fa3
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-01 19:49:53 -0300

    Update ambiente.js

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -4,5 +4,5 @@ const cars = {type:"fiat", model:"500", color:"white"}
 console.log(cars);
 cars.color = "red";
 console.log(cars);
-cars.type = "toyota";
+cars.type = "toyota";   
 console.log(cars);
