9708185605104c83d035b7c4507d5567a7797032
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-01 20:27:56 -0300

    Update ambiente.js

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -1,6 +1,12 @@
-const pessoa = {nome:"", sobrenome:"", idade:""};
+const Pessoa = {nome:"", sobrenome:"", idade:""};
+const aluno = Pessoa;
 
-pessoa.nome = "carlos";
-pessoa.sobrenome = "teixeira";
-pessoa.idade = 28;
-console.log(pessoa);
+Pessoa.nome = "carlos";
+Pessoa.sobrenome = "teixeira";
+Pessoa.idade = 28;
+
+aluno.idade = 24;
+
+console.log(Pessoa);
+console.log(aluno);
+console.log(typeof(aluno));
