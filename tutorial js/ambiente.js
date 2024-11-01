84e7c03f108e46304b483ccfcf52eaa60e9bbb95
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-01 19:41:21 -0300

    Update ambiente.js

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -5,4 +5,8 @@ console.log(cars);
 console.log(cars[3] = "mercedes");
 console.log(cars)
 console.log(cars.length);
+cars.push("gol");
+console.log(cars.length);
+cars[0] = "nada";
+console.log(cars)
 console.log(typeof(cars));
