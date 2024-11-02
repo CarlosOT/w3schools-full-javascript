6f2b768dd3e8298ecae3ef562e3a9024947571ca
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-01 21:00:01 -0300

    Update script.js

--- tutorial js/treino 7/script.js
+++ tutorial js/treino 7/script.js
@@ -4,5 +4,5 @@ function multiplicar(n1, n2){
 
 let x1 = 10;
 let x2 = 5;
-let resultado = multiplicar(x1,x2);
+let resultado = multiplicar(x1, x2);
 document.getElementById('demo').innerHTML = `${x1} * ${x2} = ${resultado}`;
