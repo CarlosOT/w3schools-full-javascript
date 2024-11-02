74e1d7a9ce8a876cf0036dc873af0e86b15ce4c9
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-02 13:40:13 -0300

    treino js tutorial

--- tutorial js/treino 9/script.js
+++ tutorial js/treino 9/script.js
@@ -6,5 +6,5 @@ let n2 = 5;
 let resultadoMultiplicacao = Multiplicar(n1, n2);
 let resultadoSoma = Somar(n1, n2);
 
-document.getElementById('demo').innerHTML = resultadoMultiplicacao;
-document.getElementById('demo2').innerHTML = resultadoSoma;
+document.getElementById('demo').innerHTML = `${n1} x ${n2} = ${resultadoMultiplicacao}`;
+document.getElementById('demo2').innerHTML = `${n1} + ${n2} = ${resultadoSoma}`;
