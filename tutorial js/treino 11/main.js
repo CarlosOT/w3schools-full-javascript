5b0be2a8f0a8caed0c5edbfbce9ac47a26952dd8
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-05 17:48:46 -0300

    treino js tutorial

--- tutorial js/treino 11/main.js
+++ tutorial js/treino 11/main.js
@@ -1,6 +1,6 @@
 import * as mds from "./modules/script.js"
 
-const age = Number(20);
+// const age = Number(20);
 
 /*
 if(age < 18){
@@ -11,4 +11,18 @@ if(age < 18){
 */
 
 //este é o operador ternário
-let liberado = (age < 18) ? mds.htmlWrite('demo', "Muito Novo Para Comprar Alcool") : mds.htmlWrite('demo', "Pode Comprar Alcool");
+// let liberado = (age < 18) ? mds.htmlWrite('demo', "Muito Novo Para Comprar Alcool") : mds.htmlWrite('demo', "Pode Comprar Alcool");
+
+// let myNumber =2;
+// while(myNumber != Infinity){
+//     window.alert(myNumber *= myNumber);
+// }
+
+
+// let x = Number.MAX_SAFE_INTEGER;
+
+let x = 20;
+let y = 20.6;
+
+mds.htmlWrite('demo', Number.isInteger(x));
+mds.htmlWrite('demo', Number.isInteger(y));
