736a0606a51696404d7c833eec9699f1d7e6fcd9
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-05 19:07:07 -0300

    treino js tutorial

--- tutorial js/treino 13/main.js
+++ tutorial js/treino 13/main.js
@@ -1,8 +1,11 @@
 import * as mds from "./modules/script.js"
 
 
-let n1 = 81.2;
+let n1 = 23.43;
 // mds.htmlWrite('demo', Math.sqrt(n1));
 // mds.htmlWrite('demo', Math.random(n1));
 // mds.htmlWrite('demo', Math.round(n1));
-mds.htmlWrite('demo', Math.ceil(n1));
+// mds.htmlWrite('demo', Math.ceil(n1));
+// mds.htmlWrite('demo', Math.min(2,4,5,1,3,4,3,1,2,3,1,31,41,2));
+mds.htmlWrite('demo', Math.max(2,4,5,1,3,4,3,1,2,3,1,31,41,2));
+
