e214f7b2a60246ba8c5fbb4b8d1b381332005b48
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-09 13:40:41 -0300

    treino js tutorial

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -1,3 +1,5 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango", "Banana", "Abacaxi"];
+let fruits = ["Banana", "Orange", "Apple", "Mango", "Banana", "Abacaxi"];
+fruits = fruits.toString().toLocaleLowerCase();
 
-console.log(fruits.lastIndexOf('Banana'));
+console.log(fruits);
+console.log(fruits.includes('banana'));
