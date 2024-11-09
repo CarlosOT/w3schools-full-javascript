7ff1adf584df5526593538795f0319211a9669f0
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-09 13:31:11 -0300

    treino js tutorial

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -1,4 +1,4 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-const newArray = fruits.slice(2);
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Abacaxi"];
+const newArray = fruits.slice(1,3);
 
 console.log(newArray);
