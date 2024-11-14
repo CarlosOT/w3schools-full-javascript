ffa3006ec73f76c45debbee66d8af647001879a6
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-14 11:07:35 -0300

    treino js tutorial

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -1,5 +1,5 @@
 const numbers = [45, 4, 9, 16, 25];
-let sum = numbers.reduce(myFunction);
+let sum = numbers.reduce(myFunction, 100);
 
 function myFunction(total, value) {
   return total + value;
