5b6d0ba506a40961f1da8c58ac3ab559ad65bd81
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-18 14:36:16 -0300

    treino js tutorial

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -1,4 +1,8 @@
-const months = ["Januar", "Februar", "Mar", "April"];
-const myMonths = months.with(2, "March");
+const q1 = ["Jan", "Feb", "Mar"];
+const q2 = ["Apr", "May", "Jun"];
+const q3 = ["Jul", "Aug", "Sep"];
+const q4 = ["Oct", "Nov", "May"];
 
-console.log(months);
+const year = [...q1, ...q2, ...q3, ...q4];
+
+console.log(year[5]);
