8a228cb4b1824ed82fabda9186871a8629cf2b12
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-19 09:45:54 -0300

    treino js tutorial

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -1,4 +1,6 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-const d = new Date("2021-03-25");
+const d = new Date();
 let month = months[d.getMonth()];
+
+console.log(month);
