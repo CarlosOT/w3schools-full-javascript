cc679bf2e55477c0a866d6d6d70b599e515c180d
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-26 09:50:34 -0300

    treino js tutorial

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -2,7 +2,8 @@ const cars = ["ford", "chevette"];
 let i = 0;
 let len = cars.length;
 let text = "";
-for (; i < len; i++) {
+for (; i < len; ) {
   text += cars[i] + " ";
+  i++;
   console.log(text);
 }
