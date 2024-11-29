2c2aac94f4a2e34b31ac4c4a3f0973dd2784f9b8
Carlos Teixeira <c.texeirajr@gmail.com>
2024-11-29 07:30:12 -0300

    treino js tutorial

--- tutorial js/ambiente.js
+++ tutorial js/ambiente.js
@@ -1,4 +1,15 @@
 const letters = new Set(['a','b','c']);
- 
+letters.add('d');
 
 console.log(typeof letters);
+console.log(letters.size);
+console.log(letters.values());
+console.log(letters.keys());
+console.log(letters);
+
+let text = '';
+for (const x of letters){
+  text += x + ' ';
+}
+
+console.log(text);
